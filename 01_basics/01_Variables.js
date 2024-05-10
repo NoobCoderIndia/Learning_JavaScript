cc25eeabcd6e6390d02f11b91fcdc48826f94842
Sanchit Sharma <99388605+NoobCoderIndia@users.noreply.github.com>
2024-05-10 12:00:45 +0000

    dataTypes

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,31 @@
+/*
+Variables are Containers for Storing Data
+
+JavaScript Variables can be declared in 4 ways:
+
+Automatically
+Using var
+Using let
+Using const
+*/
+
+const accountId = 15487936;
+let accountEmail = "NoobCoder@google.com"; // Fake email 
+var accountPassword = "789012";
+accountCity = "Delhi";
+let accountState; // undefined
+
+// accountId = 2   //const can't be chanagd 
+
+accountEmail = "nci@nci.com";
+accountPassword = "456123";
+acountCity = "Uttarpradesh";
+
+
+console.log(accountId); // cntrl + ` to open terminal
+
+/*
+Prefer not to use var because of issue in block and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,42 @@
+/* JavaScript has 8 Datatypes
+1. String
+2. Number
+3. Bigint
+4. Boolean
+5. Undefined (value not assigned yet)
+6. Null (stand alone value)
+7. Symbol (unique)
+8. Object
+
+The Object Datatype
+The object data type can contain:
+
+1. An object
+2. An array
+3. A date
+
+Note: A JavaScript variable can hold any type of data.
+*/
+
+"use strict"; // means treat all Js code as newer version
+
+// alert ( 5 * 3) // we are using nodejs, not browser
+
+console.log(7 
+    +
+     5); // code readability should be high 
+
+/* for documentation 
+1. mdn
+2. tc39.es (ecma script)
+*/
+
+let name = "Noob"; // string
+let age = 22; // number
+let isLoggedIn = false; //boolean
+let state; //undefined
+
+console.log(typeof "Noob");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
