de93941aade2ea00f49f2dc05abd9074c86626ff
Sanchit Sharma <99388605+NoobCoderIndia@users.noreply.github.com>
2024-05-11 10:22:01 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,70 @@
+// JavaScript Type Conversion
+
+// Converting Strings to Numbers
+// Converting Numbers to Strings
+// Converting Dates to Numbers
+// Converting Numbers to Dates
+// Converting Booleans to Numbers
+// Converting Numbers to Booleans
+
+
+// let age = "19";
+
+// console.log(typeof age); // or
+// console.log(typeof(age));      // string
+
+// let age = "19abc";
+// let valueInNumber = Number(age);
+// console.log(typeof valueInNumber);  //number
+// console.log(valueInNumber);  //NaN
+
+// let age = null;
+// let valueInNumber = Number(age);
+// console.log(typeof valueInNumber);  
+// console.log(valueInNumber); // 0
+
+
+// let age = undefined;
+// let valueInNumber = Number(age);
+// console.log(typeof valueInNumber);  
+// console.log(valueInNumber); // NaN\
+
+// let age = true;
+// let valueInNumber = Number(age);
+// console.log(typeof valueInNumber);  
+// console.log(valueInNumber); // true = 1; false = 0 
+
+
+// let age = "Noob";
+// let valueInNumber = Number(age);
+// console.log(typeof valueInNumber);  
+// console.log(valueInNumber); // NaN 
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); //true
+
+// let isLoggedIn = "";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); // false
+
+// let isLoggedIn = "Noob";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); // true
+
+/*
+
+"19" => 19
+"19abc" => NaN
+true => 1; false => 0
+"" => false
+"Noob" => true
+
+*/
+
+
+
+
+
+
+
